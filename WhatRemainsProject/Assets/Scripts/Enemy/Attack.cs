7f3fef9ecba46df6c18c58fd332c8df467b1c00e
Tezika <tezikazhou@gmail.com>
2019-08-26 22:52:40 -0600

    Added the enemy's attack AI in.

---
+++ WhatRemainsProject/Assets/Scripts/Enemy/Attack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace WhatRemains.Enemy.AI
+{
+    public class Attack : MonoBehaviour
+    {
+        public float dis_attack;
+
+        public Animator animator;
+
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            if (this.animator == null)
+            {
+                throw new System.NullReferenceException(nameof(this.animator));
+            }
+        }
+
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+        }
+    }
+
+}
