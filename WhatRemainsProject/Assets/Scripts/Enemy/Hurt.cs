510cab3ea1dfb2a9cc918702c74cb6354043deea
Fengkai_Zhou <tezikazhou@gmail.com>
2019-08-27 11:01:35 -0600

    Added the enemy hurt/die logic in.

--- WhatRemainsProject/Assets/Scripts/Enemy/Hurt.cs
+++ WhatRemainsProject/Assets/Scripts/Enemy/Hurt.cs
@@ -4,21 +4,61 @@ namespace WhatRemains.Enemy.AI
 {
     public class Hurt : MonoBehaviour
     {
+        public float cooldown_hurt = 0.5f;
+
+        public int health;
+
+        public GameObject enemy;
+
+        private float _timer;
+
+        private bool _bInDamaging;
+
         private void Awake()
         {
-            
+            _bInDamaging = false;
+            if (this.enemy == null)
+            {
+                throw new System.NullReferenceException(nameof(this.enemy));
+            }
         }
 
-        // Start is called before the first frame update
-        private void Start()
+        private void OnTriggerEnter(Collider other)
         {
-
+            if (other.gameObject.tag == "Sword")
+            {
+                _bInDamaging = true;
+                _timer = this.cooldown_hurt;
+            }
         }
 
-        // Update is called once per frame
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Sword")
+            {
+                _bInDamaging = false;
+            }
+        }
+
         private void Update()
         {
-
+            if (_bInDamaging)
+            {
+                _timer -= Time.deltaTime;
+                if (_timer <= 0.0f)
+                {
+                    health--;
+                    if (health <= 0)
+                    {
+                        Debug.Log("The enemy will die.");
+                        GameObject.Destroy(this.enemy);
+                    }
+                    else
+                    {
+                        _timer = this.cooldown_hurt;
+                    }
+                }
+            }
         }
     }
 
